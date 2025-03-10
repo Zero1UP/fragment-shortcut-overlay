06b8bc12f6f8c97a6df4cb906b79cbb0c83a2f40
Zero1UP <Zero1UP@gmail.com>
2025-03-09 20:53:09 -0600

    Code cleanup and version change

--- fragment-shortcut-overlay/Helpers/GameHelper.cs
+++ fragment-shortcut-overlay/Helpers/GameHelper.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using fragment_shortcut_overlay.Helpers;
-using Microsoft.Win32.SafeHandles;
+﻿using System.Collections.Generic;
 
 namespace fragment_shortcut_overlay
 {
